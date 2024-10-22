b47e69e595d4fa652bd9dc07bf7fb463ee5f6e7a
EnmanuelFL <enmanuellemos.f@gmail.com>
2024-10-22 16:09:48 +0200

    first part of the delete done

--- server/controllers/petController.ts
+++ server/controllers/petController.ts
@@ -17,15 +17,32 @@ import { Request, Response } from "supertest";
 
 //get oone id de los post
 
-export const getPet = async (req: Request, res: Response) => {
+// export const getPet = async (req: Request, res: Response) => {
+//     try {
+//       const { id } = req.params;
+//       const pet = await petsModel.findByPk(id);
+//       if (!pet) {
+//         return res.status(404).json({ message: "Pet not found" });
+//       }
+//       res.json(pet);
+//     } catch (error) {
+//       res.status(500).json({ message: error.message });
+//     }
+//   };
+
+// delete de los post
+
+export const deletePet = async (req: Request, res: Response) => {
     try {
       const { id } = req.params;
       const pet = await petsModel.findByPk(id);
       if (!pet) {
         return res.status(404).json({ message: "Pet not found" });
       }
-      res.json(pet);
+      await pet.destroy();
+      res.json({ message: "Pet deleted successfully" });
     } catch (error) {
       res.status(500).json({ message: error.message });
     }
   };
+
