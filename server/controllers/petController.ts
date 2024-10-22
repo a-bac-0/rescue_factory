df5186e16d759af439776db74cfe1e089653cd87
EnmanuelFL <enmanuellemos.f@gmail.com>
2024-10-22 15:28:35 +0200

    first part of the get

--- server/controllers/petController.ts
+++ server/controllers/petController.ts
@@ -1,1 +1,18 @@
-// hola
+
+import { Request, Response } from "supertest";
+//import { petsModel } from "../models/petsModel";
+
+
+//Get del los post
+
+export const getAllPets = async (req: Request, res: Response) => {
+    try {
+      const pets = await petsModel.findAll(); 
+      res.json(pets);
+      
+} catch (error) {
+      res.status(500).json({ message: error.message });
+    }
+  };
+
+
