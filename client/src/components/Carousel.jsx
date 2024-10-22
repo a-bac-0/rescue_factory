5398a0d65c3e12fbd14849ec06e2ddb74e21f139
jruizndev <jorsqn@gmail.com>
2024-10-22 14:56:19 +0200

    carousel ready, auto timer slides, data with elements of talbe adoptions

--- client/src/components/Carousel.jsx
+++ client/src/components/Carousel.jsx
@@ -5,68 +5,125 @@ import Arrow2 from '../assets/images/Arrow 2.svg'
 
 const Carousel = () => {
     const [currentSlide, setCurrentSlide] = useState(0)
-    const slideRef = useRef(null) // Referencia al slide actual
+    const [isSwiping, setIsSwiping] = useState(false)
+    const startX = useRef(0)
+    const diffX = useRef(0)
+    const intervalRef = useRef(null)
 
+    // Simulación base de datos de slides hasta que esté lista la base de datos real
     const slides = [
         {
+            id: 1,
             name: 'Firulais',
             age: '2 años',
             sex: 'Macho',
-            description:
+            category: 'Perros',
+            content:
                 'Firulais es un perro muy cariñoso y juguetón. Le encanta correr en el parque.',
-            image: 'https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?fit=crop&w=800&q=80',
+            url_images:
+                'https://images.unsplash.com/photo-1517423440428-a5a00ad493e8?fit=crop&w=800&q=80',
+            user_id: 1,
         },
         {
+            id: 2,
+            name: 'Tom',
+            age: '5 años',
+            sex: 'Macho',
+            category: 'Gatos',
+            content:
+                'Tom es un gato travieso que siempre está en busca de aventuras.',
+            url_images:
+                'https://gatogazzu.org/wp-content/uploads/2021/09/gato-negro-930x620.jpg',
+            user_id: 112,
+        },
+        {
+            id: 3,
             name: 'Luna',
             age: '4 años',
             sex: 'Hembra',
-            description:
+            category: 'Perros',
+            content:
                 'Luna es una perrita tranquila que ama los paseos cortos y las siestas largas.',
-            image: 'https://content.nationalgeographic.com.es/medio/2024/09/23/perro-triste-istock-kerkez_94902f27_240923142256_1280x855.jpg',
+            url_images:
+                'https://content.nationalgeographic.com.es/medio/2024/09/23/perro-triste-istock-kerkez_94902f27_240923142256_1280x855.jpg',
+            user_id: 2,
         },
         {
+            id: 4,
             name: 'Max',
             age: '3 años',
             sex: 'Macho',
-            description:
+            category: 'Perros',
+            content:
                 'Max es muy inteligente y le encanta aprender trucos nuevos.',
-            image: 'https://images.unsplash.com/photo-1560807707-8cc77767d783?fit=crop&w=800&q=80',
+            url_images:
+                'https://images.unsplash.com/photo-1560807707-8cc77767d783?fit=crop&w=800&q=80',
+            user_id: 3,
+        },
+        {
+            id: 5,
+            name: 'Misu',
+            age: '2 años',
+            sex: 'Hembra',
+            category: 'Gatos',
+            content:
+                'Misu es una gatita tierna que le encanta dormir en lugares cálidos.',
+            url_images:
+                'https://images.unsplash.com/photo-1574158622682-e40e69881006?fit=crop&w=800&q=80',
+            user_id: 223,
         },
     ]
 
-    // Función para mostrar el slide anterior
     const handlePrev = () => {
         setCurrentSlide((prev) => (prev === 0 ? slides.length - 1 : prev - 1))
     }
 
-    // Función para mostrar el siguiente slide
     const handleNext = () => {
         setCurrentSlide((prev) => (prev === slides.length - 1 ? 0 : prev + 1))
     }
 
-    // Animación con GSAP al cambiar de slide
+    // Temporizador automático para cambiar de slide en 5 segundos
     useEffect(() => {
-        const ctx = gsap.context(() => {
-            // Aparece el nuevo slide desde la derecha
-            gsap.fromTo(
-                slideRef.current,
-                { x: 300, opacity: 0 },
-                { x: 0, opacity: 1, duration: 0.8, ease: 'power3.out' }
-            )
-        }, [currentSlide])
-
-        return () => ctx.revert() // Limpieza de la animación
+        resetTimer()
+        return () => clearInterval(intervalRef.current)
     }, [currentSlide])
 
-    // Configurar intervalo para avanzar automáticamente cada 5 segundos
+    const resetTimer = () => {
+        clearInterval(intervalRef.current)
+        intervalRef.current = setInterval(handleNext, 5000)
+    }
+
+    // Animación. Transición de slides (imágenes)
     useEffect(() => {
-        const interval = setInterval(() => {
+        gsap.fromTo(
+            '.slide-img',
+            { opacity: 0, x: 100 },
+            { opacity: 1, x: 0, duration: 0.5 }
+        )
+    }, [currentSlide])
+
+    // Deslizamiento de slides con eventos táctiles
+    const handleTouchStart = (e) => {
+        setIsSwiping(true)
+        startX.current = e.touches ? e.touches[0].clientX : e.clientX
+    }
+
+    const handleTouchMove = (e) => {
+        if (!isSwiping) return
+        diffX.current =
+            startX.current - (e.touches ? e.touches[0].clientX : e.clientX)
+    }
+
+    const handleTouchEnd = () => {
+        setIsSwiping(false)
+        if (diffX.current > 50) {
             handleNext()
-        }, 5000) // Cambia cada 5 segundos (5000 milisegundos)
-
-        // Limpiar el intervalo cuando el componente se desmonte
-        return () => clearInterval(interval)
-    }, [currentSlide])
+        } else if (diffX.current < -50) {
+            handlePrev()
+        }
+        resetTimer()
+        diffX.current = 0
+    }
 
     return (
         <div className="container w-full px-4 md:px-1">
@@ -80,14 +137,12 @@ const Carousel = () => {
                     <p className="text-sm text-gray-500">
                         {slides[currentSlide].sex}
                     </p>
-                    {/* Contenedor para la edad */}
                     <div className="text-sm text-gray-500 text-left">
                         {slides[currentSlide].age}
                     </div>
                     <p className="mt-4 text-sm text-gray-500">
-                        {slides[currentSlide].description}
+                        {slides[currentSlide].content}
                     </p>
-                    {/* Ajuste del margen del botón */}
                     <button className="mt-3 bg-[#D0A24C] text-black px-4 py-2 rounded-lg w-32">
                         Leer más
                     </button>
@@ -95,16 +150,26 @@ const Carousel = () => {
 
                 {/* Contenedor de imagen y flechas */}
                 <div className="relative flex-1 flex flex-col items-center md:items-start justify-center pr-7 pl-7 md:h-[650px] lg:h-[600px]">
-                    {/* Contenedor de imagen con proporciones diferentes según dispositivo */}
-                    <div className="relative w-full h-auto" ref={slideRef}>
+                    {/* Contenedor de imagen con deslizamiento de swipe */}
+                    <div
+                        className="relative w-full h-auto"
+                        onTouchStart={handleTouchStart}
+                        onTouchMove={handleTouchMove}
+                        onTouchEnd={handleTouchEnd}
+                        onMouseDown={handleTouchStart}
+                        onMouseMove={handleTouchMove}
+                        onMouseUp={handleTouchEnd}
+                        onMouseLeave={handleTouchEnd}
+                    >
                         <div className="aspect-[5/8] md:aspect-[16/10] w-full">
                             <img
-                                className="w-full h-full object-cover rounded-lg"
-                                src={slides[currentSlide].image}
+                                className="slide-img w-full h-full object-cover rounded-lg"
+                                src={slides[currentSlide].url_images}
                                 alt={slides[currentSlide].name}
                             />
                         </div>
                     </div>
+
                     {/* Contenedor para las flechas */}
                     <div className="flex flex-row gap-3 mb-5 md:mb-2 items-center justify-center md:justify-start mt-4">
                         <button
