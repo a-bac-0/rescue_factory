2a6f1259f6ac43efe9444ed3de4010d15403d704
a-bac-0 <anca.bacria@thevallians.com>
2024-10-23 12:18:04 +0200

    FIXED route Historias de Adopciones

--- client/src/pages/HistoriasDeAdopciones.jsx
+++ client/src/pages/HistoriasDeAdopciones.jsx
@@ -1,6 +1,3 @@
-<<<<<<< HEAD
-import React, { useEffect, useState } from 'react';
-=======
 import React from 'react'
 
 const HistoriasDeAdopciones = () => {
@@ -8,4 +5,3 @@ const HistoriasDeAdopciones = () => {
 }
 
 export default HistoriasDeAdopciones
->>>>>>> b5e2c45c9931913895008cdb7db1a8066b1a8d7c
