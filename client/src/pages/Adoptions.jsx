a41e68ec239ef4ad6f8d16408a53e097f55d4dc7
jruizndev <jorsqn@gmail.com>
2024-10-23 10:40:16 +0200

    hero

--- client/src/pages/Adoptions.jsx
+++ client/src/pages/Adoptions.jsx
@@ -9,6 +9,18 @@ const Adoptions = () => {
                 alt="Header Adoptions"
                 className="w-full h-auto"
             />
+            <div>
+                <h1 className="font-i">RESCATA</h1>
+                <p>
+                    En nuestra protectora, estamos comprometidos con brindarte
+                    la oportunidad de cambiar una vida. Cada perro y gato que
+                    encontrarás aquí está esperando un hogar amoroso y una
+                    familia que lo cuide. Nuestro equipo se asegura de que todos
+                    los animales estén sanos, vacunados y listos para ser parte
+                    de tu hogar. Adopta y dale una segunda oportunidad a un ser
+                    lleno de cariño.
+                </p>
+            </div>
         </div>
     )
 }
