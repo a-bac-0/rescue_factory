b45638749b226ce33dc5f039cf89e32a2d151932
Ana Sole <anasole@Ana-Maria-Sole.home>
2024-10-23 21:10:55 +0200

    Update of text in Home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -15,14 +15,24 @@ const Home = () => {
             <div>
                 <h1 className="font-i">EL REFUGIO</h1>
                 <p>
-                    En nuestra protectora, estamos comprometidos con brindarte
-                    la oportunidad de cambiar una vida. Cada perro y gato que
-                    encontrarás aquí está esperando un hogar amoroso y una
-                    familia que lo cuide. Nuestro equipo se asegura de que todos
-                    los animales estén sanos, vacunados y listos para ser parte
-                    de tu hogar. Adopta y dale una segunda oportunidad a un ser
-                    lleno de cariño.
+                En el corazón de nuestra comunidad, Rescue Factory se alza como un faro de esperanza para aquellos amigos de cuatro patas que más lo necesitan. Somos más que un simple refugio; somos una familia dedicada a transformar vidas, tanto de animales como de personas, a través del amor incondicional y el compromiso inquebrantable.
                 </p>
+                <h2>Lo Que Hacemos</h2>
+                <ul>
+                    <li>Rescatamos a perros y gatos en situación de vulnerabilidad</li>
+                    <li>Proporcionamos atención veterinaria completa</li>
+                    <li>Ofrecemos un ambiente seguro y amoroso durante su recuperación</li>
+                    <li>Trabajamos incansablemente en encontrar hogares perfectos para cada uno de nuestros residentes</li>
+                    <li>Realizamos un cuidadoso proceso de adopción para asegurar el mejor match entre mascotas y familias</li>
+                </ul>
+                <h2>Únete a Nuestra Causa</h2>
+                <p>
+                Rescue Factoría es más que un refugio; somos una comunidad de amantes de los animales comprometidos con hacer del mundo un lugar mejor para nuestros amigos peludos. Ya sea como adoptante, voluntario o donante, hay muchas formas de ser parte de esta hermosa misión.
+                </p>
+                <p>
+                Porque creemos que cada vida peluda merece una segunda oportunidad, y cada hogar se hace más feliz con el amor incondicional de una mascota rescatada.
+                </p>
+                <i>En Rescue Factoría, no solo salvamos vidas, creamos familias.</i>
             </div>
         </div>
     )
