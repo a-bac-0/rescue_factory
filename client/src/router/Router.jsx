61ee86732685372a50af6e17ed0c886befa44d03
a-bac-0 <anca.bacria@thevallians.com>
2024-10-23 10:33:56 +0200

    EDIT: Layout and Router

--- client/src/router/Router.jsx
+++ client/src/router/Router.jsx
@@ -1,1 +1,49 @@
-//hola
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home.jsx";
+import Layout from "../layout/Layout";
+import Adopciones from "../pages/Adopciones.jsx";
+import Noticias from "../pages/Noticias.jsx";
+import Contacto from "../pages/Contacto.jsx"
+import Registro from "../pages/Registro.jsx";
+import HistoriasDeAdopción from "../pages/HistoriasDeAdopción.jsx";
+
+
+
+
+export const router = createBrowserRouter([{
+    
+    path : '/',
+    element: <Layout/>,
+    children: [
+        {
+            index: true,
+            element: <Home/>
+    },
+    {
+        path: 'login',
+        element: <Login/>
+    },
+    {
+        path:'adopciones',
+        element: <Adopciones/>
+    },
+    {
+        path:'noticias',
+        element: <Noticias/>
+    },
+    {
+        path:'contacto',
+        element: <Contacto/>
+    },
+    {
+        path:'registro',
+        element: <Registro/>
+
+    },
+    {
+        path:'historiasDeAdopcion',
+        element: <HistoriasDeAdopción/>
+    },
+]
+
+}])
