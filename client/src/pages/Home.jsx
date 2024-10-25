facafda8ecb13d2476dc58a437e16467baeb3757
Ana Sole <anasole@Ana-Maria-Sole.home>
2024-10-25 11:00:51 +0200

    Carousel margins fixed on both desktop and mobile

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -57,7 +57,7 @@ const Home = () => {
         <div className="absolute -top-[2.5rem] sm:-top-[2.5rem] left-0 w-full h-full flex justify-center ">
           <h1 className="font-black text-5xl text-[#31442C]">ADOPCIONES</h1>
         </div>
-        <section className="m-6">
+        <section className="w-full min-w-[314px] max-w-[850px] mx-auto px-8 sm:px-4 md:px-14 lg:px-20 mb-36">
           <Carousel />
         </section>
       </section>
