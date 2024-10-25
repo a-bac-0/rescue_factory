a90195a7cdefe5078dfeb7fb154a40fd7500f3a2
Ana Sole <anasole@Ana-Maria-Sole.home>
2024-10-25 12:19:19 +0200

    Adding styles to the button MAS ADOPCIONES

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-
-
+import React from "react";
 
 const MyButton = () => {
-    return (
-      <button>Soy un botón</button>
-    );
-  }
+  return (
+      <button className="w-[345px] h-[59px] px-[80px] py-[17px] bg-[#d1b85e] rounded-lg border-2 border-neutral-100 justify-center items-center gap-2.5 inline-flex text-[#31442c] text-lg font-bold font-['Inter'] leading-normal">
+        MÁS ADOPCIONES
+      </button>
+  );
+};
 
-  export default MyButton
+export default MyButton;
