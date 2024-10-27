4cd751eaf581be40ee9eb18b5ce565863b531d7c
jruizndev <jorsqn@gmail.com>
2024-10-27 13:29:46 +0100

    card component ready to connect to database

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -3,21 +3,25 @@ import React from 'react'
 const CHAR_LIMIT = 80
 
 const Card = () => {
-    // Función para recortar el texto sin cortar palabras
+    const adoptions = {
+        name: 'Nube',
+        age: '2 años',
+        sex: 'Hembra',
+        content:
+            'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, dicta sunt explicabo.',
+        url_images:
+            'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwyXeKDN29AmZgZPLS7n0Bepe8QmVappBwZCeA3XWEbWNdiDFB',
+    }
+
     const truncateContent = (text) => {
-        if (text.length <= CHAR_LIMIT) {
-            return text
-        }
+        if (text.length <= CHAR_LIMIT) return text
 
         const words = text.split(' ')
         let truncatedText = ''
         for (let word of words) {
-            if ((truncatedText + word).length > CHAR_LIMIT) {
-                break
-            }
+            if ((truncatedText + word).length > CHAR_LIMIT) break
             truncatedText += word + ' '
         }
-
         return truncatedText.trim() + '...'
     }
 
@@ -25,23 +29,21 @@ const Card = () => {
         <div className="bg-white items-center w-[314px] h-[435px] flex flex-col rounded-md shadow-md cursor-pointer hover:scale-105 transition-transform duration-300">
             <div className="flex flex-col justify-center items-center w-[85%]">
                 <h1 className="w-full h-[18px] text-left pt-8 pb-4 font-inter font-bold text-[18px] ">
-                    NUBE
+                    {adoptions.name}
                 </h1>
                 <h2 className="w-full text-left font-inter pt-3 text-[15px]">
-                    Edad
+                    {adoptions.age}
                 </h2>
-                <h2 className="w-full text-left font-inter pt-1 text-[15px]">
-                    Sexo
+                <h2 className="w-full text-left font-inter pb-3 text-[15px]">
+                    {adoptions.sex}
                 </h2>
                 <p className="w-full text-left font-inter pt-1 text-[15px]">
-                    {truncateContent(
-                        'Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, dicta sunt explicabo.'
-                    )}
+                    {truncateContent(adoptions.content)}
                 </p>
             </div>
             <img
-                src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTwyXeKDN29AmZgZPLS7n0Bepe8QmVappBwZCeA3XWEbWNdiDFB"
-                alt="Dog"
+                src={adoptions.url_images}
+                alt={adoptions.name}
                 className="w-[85%] h-[200px] object-cover rounded-md mt-4"
             />
         </div>
