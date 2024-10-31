b2e9d80119d9b7c0c7526ad5d7e33ff1d244e433
EnmanuelFL <enmanuellemos.f@gmail.com>
2024-10-31 12:10:38 +0100

    ADD: crete method POST and GetOneId of the Adoption

--- server/controllers/adoptionController.ts
+++ server/controllers/adoptionController.ts
@@ -12,3 +12,40 @@ export const getAdoptions = async (req: Request, res: Response) => {
     }
 }
 
+// Geto one de una adopcion
+
+export const getOneAdoption = async (req: Request, res: Response) => {
+    try {
+        const { id } = req.params
+        const adoptionOne = await adoptionModel.findByPk(id)
+
+        if (adoptionOne) {
+            res.json(adoptionOne)
+        }
+        
+    } catch (error) {
+        console.log('No se pudo obtener la informacion de esta adopcion', error)
+        
+    }
+}
+
+// Post de una adopcion 
+
+export const createAdoption = async (req: Request, res: Response) => {
+    try {
+        const {name, age, sex, category, content, url_images, user_id} = req.body;
+        const adoption = await adoptionModel.create({
+            name,
+            age, 
+            sex,
+            category,
+            content,
+            url_images,
+            user_id,
+        })
+            res.json(adoption)
+
+    } catch (error) {
+            console.log('Erro al subir la adopcion', error)
+    }
+}
