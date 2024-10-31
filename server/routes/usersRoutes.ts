1465eb6d61b101f2f5a97b77e63822ffd3bb6f69
jfigueira87 <jfigueira@gmail.com>
2024-10-31 12:35:49 +0100

    ADD:configuration  user routes

--- server/routes/usersRoutes.ts
+++ server/routes/usersRoutes.ts
@@ -1,1 +1,11 @@
-//Users
+import { deleteUser, getAllUsers, getUserById, updateUser } from "../controllers/userController";
+import express from "express";
+
+const bookRouter = express.Router();
+
+bookRouter.get('/users', getAllUsers);
+bookRouter.get('/users/:id', getUserById);
+bookRouter.delete('/users/:id', deleteUser);
+bookRouter.put('/users/:id', updateUser);
+
+export default bookRouter;
