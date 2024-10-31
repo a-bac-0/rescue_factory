956878582c31439d35dd172c6a0a86da37ee2497
Ana Sole <anasole@Ana-Maria-Sole.home>
2024-10-31 16:28:18 +0100

    Style added to ta title as well as margins and paddings

--- client/src/pages/Contacto.jsx
+++ client/src/pages/Contacto.jsx
@@ -17,10 +17,10 @@ const Contacto = () => {
         alt="Header Home"
         className="w-full h-auto object-cover block md:hidden"
       />
-        <section className="w-[80%]">
- 
-    <h1>Contáctanos</h1>
-    <p>Llena el formulario para ponerte en contacto con nosotros.</p>
+       <section className="w-[80%] mx-auto px-8 sm:px-4 md:px-14 lg:px-20 mb-36 text-[#F5F5F5]">
+        <h1 className="font-bold mt-10 mb-8 sm:my-6 md:my-10 text-5xl lg:text-7xl">
+          CONTACTO
+        </h1>
     <ContactForm />
     </section>
   </main>
