c62d87cc5cc999144c0f12520887870f268e64b4
Ana Sole <anasole@Ana-Maria-Sole.home>
2024-10-31 15:27:33 +0100

    Header images added to the contact page, both web and mobile.

--- client/src/pages/Contacto.jsx
+++ client/src/pages/Contacto.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import ContactForm from '../components/ContactForm';
+import HeaderContactImg from '../assets/images/Header_contact.png';
+import HeaderContactMobile from '../assets/images/Header_contact_mobile.png';
 
 const Contacto = () => {
     return  (
-    <div>
+    <main className="min-h-screen w-full m-0 bg-[#76816A] font-['Inter']">
+        <img
+        src={HeaderContactImg}
+        alt="Header Home"
+        className="w-full h-auto object-cover hidden md:block"
+      />
+
+      <img
+        src={HeaderContactMobile}
+        alt="Header Home"
+        className="w-full h-auto object-cover block md:hidden"
+      />
+        <section className="w-[80%]">
+ 
     <h1>Contáctanos</h1>
     <p>Llena el formulario para ponerte en contacto con nosotros.</p>
     <ContactForm />
-  </div>
+    </section>
+  </main>
 );
 }
 
