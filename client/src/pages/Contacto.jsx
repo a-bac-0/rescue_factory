b0cf8186265dca318185ac58a6c2e5d94578b18b
AnaMaria-Sole <anamgarcia.a@yahoo.com>
2024-11-04 15:11:10 +0100

    Update of contact page

--- client/src/pages/Contacto.jsx
+++ client/src/pages/Contacto.jsx
@@ -18,7 +18,7 @@ const Contacto = () => {
         className="w-full h-auto object-cover block md:hidden"
       />
        <section className="w-[80%] mx-auto px-8 sm:px-4 md:px-14 lg:px-20 mb-36 text-[#F5F5F5]">
-        <h1 className="font-bold mt-10 mb-8 sm:my-6 md:my-10 text-5xl lg:text-7xl">
+        <h1 className="font-bold mt-10 mb-8 sm:my-6 md:my-10 text-4xl lg:text-7xl">
           CONTACTO
         </h1>
     <ContactForm />
