4a54b1ca91ae49bc1d386b0f1bcc289940d9df3b
jruizndev <jorsqn@gmail.com>
2024-11-05 13:57:14 +0100

    article error solved

--- client/src/pages/Article.jsx
+++ client/src/pages/Article.jsx
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import { getAPostsById } from '../services/PostsServices'
 import { getAdoptionsById } from '../services/AdoptionsServices'
-import { getAPostsById } from '../services/PostsServices'
 import { getUsersById } from '../services/UsersServices'
 
 const Article = () => {
