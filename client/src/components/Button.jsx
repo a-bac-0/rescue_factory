659813da0bac896ad3972639699e3c256d3703aa
AnaMaria-Sole <anamgarcia.a@yahoo.com>
2024-11-07 17:38:11 +0100

    On the button

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,13 +1,13 @@
 import React from "react";
 
 const MyButton = ({ label, onClick }) => {
-    
-  return (
 
-      <button  onClick={onClick} className="w-[345px] h-[59px] px-[80px] py-[17px] bg-[#d1b85e] rounded-lg border-2 border-neutral-100 justify-center items-center gap-2.5 inline-flex text-[#31442c] text-lg font-bold font-['Inter'] leading-normal hover:bg-[#d6c99e] transition duration-300focus:outline-none focus:ring focus:ring-neutral-100 active:bg-[#d1b85e]">
+    return (
+
+        <button  onClick={onClick} className="w-[345px] h-[59px] px-[80px] py-[17px] bg-[#d1b85e] rounded-lg border-2 border-neutral-100 justify-center items-center gap-2.5 inline-flex text-[#31442c] text-lg font-bold font-['Inter'] leading-normal hover:bg-[#d6c99e] transition duration-300focus:outline-none focus:ring focus:ring-neutral-100 active:bg-[#d1b85e]">
         {label}
       </button>
-  );
+    );
 };
 
-export default MyButton;
+export default MyButton
