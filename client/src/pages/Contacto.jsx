b7e9ba1b0f3d631a9672a7c0f145fe89b16c26a5
Ana Sole <anasole@Ana-Maria-Sole.home>
2024-11-08 23:31:37 +0100

    applied styles to contact form

--- client/src/pages/Contacto.jsx
+++ client/src/pages/Contacto.jsx
@@ -17,14 +17,16 @@ const Contacto = () => {
         alt="Header Home"
         className="w-full h-auto object-cover block md:hidden"
       />
-       <section className="w-[80%] mx-auto px-8 sm:px-4 md:px-14 lg:px-20 text-[#F5F5F5]">
+
+<section className="mx-auto">
+       <section className=" mx-auto px-8 sm:px-4 md:px-14 lg:px-20 text-[#F5F5F5]">
         <h1 className="font-bold mt-10 mb-8 sm:my-6 md:my-10 text-4xl lg:text-7xl">
           CONTACTO
         </h1>
-        <section className="flex justify-center">
-        <ContactForm />
         </section>
-    
+        <section className="sm:w-[100%] lg:w-[80%] flex justify-center">
+        <ContactForm />
+    </section>
     </section>
   </main>
 );
