bbe857cc26fcb792abd8eadcb46e7d858d3c672e
Ana Sole <anasole@Ana-Maria-Sole.home>
2024-11-08 22:30:01 +0100

    Text width changed

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -41,7 +41,7 @@ const Home = () => {
         className="w-full h-auto object-cover block md:hidden"
       />
 
-      <section className="w-[80%] mx-auto px-8 sm:px-4 md:px-14 lg:px-20 mb-36 text-[#F5F5F5]">
+      <section className="w-95%] mx-auto px-8 sm:px-4 md:px-14 lg:px-20 mb-36 text-[#F5F5F5]">
         <h1 className="font-bold mt-10 mb-8 sm:my-6 md:my-10 text-5xl lg:text-7xl">
           EL REFUGIO
         </h1>
