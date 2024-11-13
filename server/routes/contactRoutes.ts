a43af9c184e1caad178a2a2e3b0bd7df16e5fd85
Ana Sole <anasole@Ana-Maria-Sole.home>
2024-11-13 21:20:32 +0100

    contactRoutes modified

--- server/routes/contactRoutes.ts
+++ server/routes/contactRoutes.ts
@@ -6,15 +6,7 @@ import contactMessageModel from '../models/contactModel';
 
 const contactRouter = express.Router();
 
-contactRouter.get('/', verifyTokenMiddleware, async (req, res) => {     
-    try {
-        const messages = await contactMessageModel.findAll();
-        res.status(200).json(messages);
-    } catch (error) {
-        console.error('Error fetching contact messages:', error);
-        res.status(500).json({ message: 'Error fetching contact messages' });
-    }
-});    
+contactRouter.get('/', getContacts);  
 contactRouter.get('/:id', verifyTokenMiddleware, getContactById);    
 contactRouter.post('/', verifyTokenMiddleware, createContact);
 contactRouter.delete('/:id', verifyTokenMiddleware, authorizeRole(['admin']), deleteContact);
